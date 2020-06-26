bf058a9727f6227a36602e06029e1c2ccee94ba8
benod44 <binod.batech@gmail.com>
2020-06-26 09:29:26 -0400

    Update SpringMongoMappingApplication.java

--- src/main/java/com/ben/springboo/mapping/SpringMongoMappingApplication.java
+++ src/main/java/com/ben/springboo/mapping/SpringMongoMappingApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringMongoMappingApplication {
 
 	public static void main(String[] args) {
+		System.out.println("this is auto build");
 		SpringApplication.run(SpringMongoMappingApplication.class, args);
 	}
 
